eca2fd44a06a256bf7267a75e51b7200245a1796
Chiranthchiru65 <chiranthchiru65@gmail.com>
2024-07-08 18:15:11 +0530

    used useParam to store state in the url

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Login from "./pages/Login.jsx";
 import AppLayout from "./pages/AppLayout.jsx";
 import CityList from "./components/CityList.jsx";
 import CountryList from "./components/CountryList.jsx";
+import City from "./components/City.jsx";
 
 const BASE_URL = "http://localhost:8100";
 
@@ -48,6 +49,7 @@ function App() {
               path="cities"
               element={<CityList cities={cities} isLoading={isLoading} />}
             />
+            <Route path="cities/:id" element=<City /> />
             <Route
               path="countries"
               element={<CountryList cities={cities} isLoading={isLoading} />}
